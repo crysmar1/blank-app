f6d6ae0e94dc18a7e1fe718f599e3acb0ecf925e
Cristiano Ribeiro <97628073+crysmar1@users.noreply.github.com>
2025-01-09 23:19:17 +0000

    first app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Estoque disponível")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Lista de estoque disponível para novas vendas. "
 )
