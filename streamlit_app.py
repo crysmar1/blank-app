d108a6f3dbcbeafcc973831f6dd9d6f94bd32cd9
Cristiano Ribeiro <97628073+crysmar1@users.noreply.github.com>
2025-01-09 23:38:19 +0000

    add context

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 st.title("🎈 Estoque disponível")
 st.write(
     "Lista de estoque disponível para novas vendas. "
 )
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+})
+
+df
